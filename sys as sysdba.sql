00759bff1afedbc5a38e19f3b8f307afa38b71e8
kthss01 <kthss01@naver.com>
2021-06-24 21:44:34 +0900

    JOIN 과제

--- sys as sysdba.sql
+++ sys as sysdba.sql
@@ -6,3 +6,8 @@ GRANT RESOURCE, CONNECT TO EMPLOYEE;
 
 --select * from nls_session_parameters
 --        where PARAMETER in  ('NLS_LANGUAGE','NLS_DATE_FORMAT','NLS_DATE_LANGUAGE');
+
+CREATE USER kh IDENTIFIED BY oracle;
+GRANT CONNECT, RESOURCE TO kh;
+SELECT username from dba_users;
+SELECT username, account_status FROM dba_users WHERE username='KH';
